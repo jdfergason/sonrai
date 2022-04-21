373acdf3c5e5f40e911c0ca9421999475cf5f67c
jdfergason <jeremy.fergason@protonmail.com>
2022-04-21 15:01:11 +0000

    Add additional tables

---
+++ db/migrations/000002_create_producers_table.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE producer (
+    id UUID PRIMARY KEY,
+    user TEXT,
+    slug TEXT,
+    name TEXT,
+    description TEXT,
+    kind TEXT,
+    command TEXT,
+    arguments JSONB,
+    environment JSONB,
+    secrets JSONB,
+    schedule TEXT,
+    lastRun TIMESTAMP,
+    recordTypes JSONB,
+    created TIMESTAMP,
+    updated TIMESTAMP
+);
---
+++ db/migrations/000003_create_secrets_table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE secrets (
+    user TEXT,
+    name TEXT,
+    secret TEXT
+);
