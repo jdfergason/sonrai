c004e2dea09b945a950416029499ef77c6e46314
jdfergason <jeremy.fergason@protonmail.com>
2022-04-21 19:07:23 +0000

    Add database schema

--- db/migrations/000004_create_transformers_table.up.sql
+++ db/migrations/000004_create_transformers_table.up.sql
@@ -1,15 +1,15 @@
-CREATE TABLE producer (
+CREATE TABLE transformer (
     id UUID PRIMARY KEY,
     user TEXT,
     slug TEXT,
     name TEXT,
     description TEXT,
     kind TEXT,
+    repository TEXT,
     command TEXT,
     arguments JSONB,
     environment JSONB,
     secrets JSONB,
-    log_configuration JSONB,
     schedule TEXT,
     lastRun TIMESTAMP,
     recordTypes JSONB,
